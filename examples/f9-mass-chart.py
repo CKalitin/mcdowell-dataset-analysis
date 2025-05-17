f8f79f4eee95587ac4de67d44a8bd7494b839641
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-05-16 18:43:57 -0700

    Pretty f9 mass chart, all formatted

--- examples/f9-mass-chart.py
+++ examples/f9-mass-chart.py
@@ -15,8 +15,8 @@ mda.Filters.filter_by_launch_vehicle_family(dataset.launch, 'Falcon9')  # Filter
 orbits = ['LEO', 'SSO', 'MEO', 'GTO', 'GEO', 'HEO', 'BEO']
 bins = [0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000,
         11000, 12000, 13000, 14000, 15000, 16000, 17000, 18000, 19000, 20000]
-mass_labels = ['0-1T', '1-2T', '2-3T', '3-4T', '4-5T', '5-6T', '6-7T', '7-8T', '8-9T', '9-10T',
-               '10-11T', '11-12T', '12-13T', '13-14T', '14-15T', '15-16T', '16-17T', '17-18T', '18-19T', '19-20T']
+mass_labels = ['0-1t', '1-2t', '2-3t', '3-4t', '4-5t', '5-6t', '6-7t', '7-8t', '8-9t', '9-10t',
+               '10-11t', '11-12t', '12-13t', '13-14t', '14-15t', '15-16t', '16-17t', '17-18t', '18-19t', '19-20t']
 
 # Bin data by orbit
 orbit_counts = {}
@@ -44,11 +44,57 @@ print("CSV file 'f9_mass_by_orbit.csv' has been created.")
     output_path='examples/outputs/f9_mass_by_orbit.png'
 )"""
 
+orbit_colors = {
+    'LEO': '#ffc000',
+    'SSO': '#ffd966',
+    'MEO': '#cc0000',
+    'GTO': '#3d85c6',
+    'GEO': '#1155cc',
+    'HEO': '#34495e',
+    'BEO': '#3c4043'
+}
+
 fig = px.histogram(output_df,
                    x=output_df.index,
                    y=output_df.columns,
-                   title='Falcon 9 Launches: Payload Mass by Orbit',
+                   title='<b>Falcon 9 Launches: Payload Mass by Orbit</b><br><sup>Christopher Kalitin 2025 - Data Cutoff: May 16 2025</sup>',
                    labels={'x': 'Payload Mass Range (Tons)', 'y': 'Number of Launches'},
-                   barmode='group')
+                   barmode='stack',
+                   color_discrete_map=orbit_colors,
+                   )
 
-pio.write_image(fig, 'examples/outputs/f9_mass_by_orbit.png', format='png', width=800, height=600)
+fig.update_layout(
+    # Font settings
+    font=dict(family='Arial, sans-serif', size=20, color="#000000"),
+    title=dict(font=dict(size=40, family='Arial, sans-serif', color="#000000"), x=0.025, xanchor="left"),
+    # Background and borders
+    plot_bgcolor="white",
+    paper_bgcolor="white",
+    # Gridlines
+    xaxis=dict(
+        gridcolor="rgba(200, 200, 200, 0.5)",
+        linecolor="#000000",
+        tickangle=45,
+        title_font=dict(size=24, family="Arial, sans-serif"),
+        title_text="Payload Mass Range (tonnes)",
+    ),
+    yaxis=dict(
+        gridcolor="rgba(200, 200, 200, 0.5)",
+        linecolor="#000000",
+        title_font=dict(size=24, family="Arial, sans-serif"),
+        title_text="Number of Launches",
+    ),
+    # Legend
+    showlegend=True,
+    legend=dict(
+        font=dict(size=24, family="Arial, sans-serif"),
+        bordercolor="white",
+        borderwidth=1,
+        bgcolor="white",
+        title=dict(text=""),  # Add this line to remove "variable" text
+    ),
+    # Remove hover effects and other embellishments
+    hovermode="x",
+)
+
+pio.write_image(fig, 'examples/outputs/f9_mass_by_orbit.png', format='png', width=1280, height=720)
