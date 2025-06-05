d39e476197208d6f52aa54fafd4721ded740af2b
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-06-04 18:45:14 -0700

    moar f9 charts

--- examples/f9_charts.py
+++ examples/f9_charts.py
@@ -4,16 +4,28 @@ scg.launches_vs_mass_by_orbit(
     launch_vehicle_simplified_name='Falcon 9',
     mass_step_size_kg=1000,
     chart_title_prefix='Falcon 9',
-    output_prefix='f9',
-    launch_vehicle_family=None
+    output_prefix='f9'
 )
 
 scg.total_mass_vs_mass_by_orbit(
     launch_vehicle_simplified_name='Falcon 9',
     mass_step_size_kg=1000,
     chart_title_prefix='Falcon 9',
-    output_prefix='f9',
-    launch_vehicle_family=None
+    output_prefix='f9'
+)
+
+scg.launches_vs_mass_by_general_launch_payload_type(
+    launch_vehicle_simplified_name='Falcon 9',
+    mass_step_size_kg=1000,
+    chart_title_prefix='Falcon 9',
+    output_prefix='f9'
+)
+
+scg.total_mass_vs_mass_by_general_launch_payload_type(
+    launch_vehicle_simplified_name='Falcon 9',
+    mass_step_size_kg=1000,
+    chart_title_prefix='Falcon 9',
+    output_prefix='f9'
 )
 
 scg.launches_vs_month_by_general_launch_payload_type(
