75341f57a7b73efd9d3bd989876896d101720989
Christopher Kalitin <Christopher.Kalitin@gmail.com>
2025-08-07 20:56:40 -0700

    LVs ported to single script & validated

--- examples/launch_vehicles.py
+++ examples/launch_vehicles.py
@@ -1,6 +1,7 @@
 import standard_chart_generation as scg
+from datetime import datetime
 
-def generate(chart_types, vehicle_name, display_name, file_name, year_x_tick_step_size, month_x_tick_step_size, mass_step_size=1000):
+def generate(chart_types, vehicle_name, display_name, file_name, year_x_tick_step_size=5, month_x_tick_step_size=60, mass_step_size=1000):
     """ Generates charts for a given launch vehicle using specified chart types.
 
     Args:
@@ -38,5 +39,18 @@ def generate(chart_types, vehicle_name, display_name, file_name, year_x_tick_ste
             mass_step_size_kg=mass_step_size,
             year_x_tick_step_size=year_x_tick_step_size,
         )
-        
+
+start_time = datetime.now()
+print("Start time: ", start_time)
+
+generate("single scatter", "Falcon 9", "Falcon 9", "f9", 1, 12)
+generate("single", "Falcon Heavy", "Falcon Heavy", "f9h", 1, 12)
+generate("single family", ['Atlas 1', 'Atlas 2', 'Atlas 3', 'Atlas 5'], "Atlas Orbital", "atlas")
 generate("single scatter", "Electron", "Electron", "electron", 1, 12, 10)
+generate("single family scatter", ['Titan 2', 'Titan 3', 'Titan 4'], "Titan Orbital", "titan")
+generate("single", "Soyuz", "Soyuz", "soyuz")
+generate("single family", ['Delta 1', 'Delta 2', 'Delta 3', 'Delta 4M', 'Delta 4H'], "Delta Orbital", "delta")
+generate("single family", ["Ariane 1", "Ariane 2", "Ariane 3", "Ariane 4", "Ariane 5", "Ariane 6"], "Ariane", "ariane")
+
+print("End time: ", datetime.now())
+print("Elapsed time: ", datetime.now() - start_time)
